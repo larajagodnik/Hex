2493e92d32120b283a297914c45a952b9182cebf
Martin Kokošinek <koki.549@gmail.com>
2020-05-29 13:53:09 +0200

    Update Vodja.java

--- Hex/Tekmovanje/src/vodja/Vodja.java
+++ Hex/Tekmovanje/src/vodja/Vodja.java
@@ -20,6 +20,7 @@ public class Vodja {
 	public static gui.GlavnoOkno okno;
 	public static boolean clovekNaVrsti = false;
 	public static boolean zmaga;
+	public static Inteligenca racunalnikovaInteligenca = new Inteligenca();
 	
 	public static void igramoNovoIgro () {
 		zmaga = false;
