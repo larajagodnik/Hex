f1586349e27b4bc2503749d095b009a694ac378f
Martin Kokošinek <koki.549@gmail.com>
2020-05-29 13:51:38 +0200

    Update Inteligenca.java

--- Hex/Tekmovanje/src/inteligenca/Inteligenca.java
+++ Hex/Tekmovanje/src/inteligenca/Inteligenca.java
@@ -1,12 +1,48 @@
 package inteligenca;
 
-import splosno.KdoIgra;
+import java.util.LinkedList;
+import java.util.Random;
 
-public class Inteligenca extends KdoIgra{
+import logika.Igra;
+import logika.Plosca;
+import logika.Polje;
+import splosno.KdoIgra;
+import splosno.Koordinati;
+
+public class Inteligenca extends KdoIgra {
+
 	public Inteligenca() {
-		super("Lara & Martin");
+		super("random");
 	}
-	public Koordinati izberiPotezo (Igra igra) {
-		
+	
+	boolean prazen = true;
+	
+	public Koordinati izberiPotezo(Igra igra) {
+		for (int i=0;i<Plosca.N;i++) {
+			for (int j=0; j<Plosca.N;j++) {
+				if (igra.plosca.plosca[i][j]!=Polje.prazno) {
+					prazen = false;
+				}
+			}
+		}
+		if (prazen) {
+			if(Plosca.N%2==0) {
+				Koordinati p = new splosno.Koordinati((Plosca.N-2)/2,(Plosca.N-2)/2);
+				prazen = true;
+				return p;
+			}
+			else{
+				Koordinati p = new Koordinati((Plosca.N-1)/2,(Plosca.N-1)/2);
+				prazen = true;
+				return p;
+			}
+		}
+		else {
+			Random r = new Random();
+			LinkedList<Koordinati> poteze = (LinkedList<Koordinati>) igra.moznePoteze();
+			Koordinati p = poteze.get(r.nextInt(poteze.size()));
+			prazen = true;
+			return p;
+		}
 	}
 }
