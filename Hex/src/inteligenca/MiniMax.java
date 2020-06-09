440dcaec7d889a010aef9219a95e4ef7ffd15449
marsovc1 <koki.549@gmail.com>
2020-06-09 04:16:33 +0200

    hudo neuspeli poskus

--- Hex/src/inteligenca/MiniMax.java
+++ Hex/src/inteligenca/MiniMax.java
@@ -5,122 +5,39 @@ import java.util.List;
 
 import logika.Igra;
 import logika.Igralec;
-import logika.Stanje;
 import splosno.Koordinati;
 import splosno.KdoIgra;
 
 
 public class MiniMax extends KdoIgra {
-	
-	private static final int ZMAGA = Integer.MAX_VALUE; // vrednost zmage
-	private static final int ZGUBA = -ZMAGA;  // vrednost izgube
-
-
-	private int globina;
+	public static Koordinati poteza;
 	
 	public MiniMax () {
 		super("Skupina instruktorjev"); 
-		this.globina = 4;
 	}
 	
 	public MiniMax (int globina) {
-		super("Skupina instruktorjev" + globina); 
-		this.globina = globina;
+		super("Skupina instruktorjev");
 	}
 	
 	// izbere najboljso potezo
 	public Koordinati izberiPotezo (Igra igra) {
-		System.out.println("odigral sem " + alphabetaPoteze(igra, this.globina, ZGUBA, ZMAGA, igra.naPotezi()).poteza);
-		return alphabetaPoteze(igra, this.globina, ZGUBA, ZMAGA, igra.naPotezi()).poteza;
-	}
-	
-	public static OcenjenaPoteza alphabetaPoteze(Igra igra, int globina, int alpha, int beta, Igralec jaz) {
-		int ocena;
-				
-		// Če sem računalnik, maksimiramo oceno z začetno oceno ZGUBA
-		// Če sem pa človek, minimiziramo oceno z začetno oceno ZMAGA
-		//System.out.println("na potezi " + igra.naPotezi() );
-		if (igra.naPotezi() == jaz) {
-			ocena = ZGUBA;
+		List<Koordinati> poteze = igra.moznePoteze();
+		Igralec igralec = igra.naPotezi;
+		for(int i=0; i<poteze.size();i++) {
+			int vrednost = OceniPozicijo.oceniPozicijo(igra, igralec);
+			System.out.println(vrednost);
+			if(vrednost==1 && igralec==Igralec.rdeci) {
+				return(poteze.get(i));
+			}
+			else if(vrednost==-1 && igralec==Igralec.modri) {
+				return(poteze.get(i));
+			}
+			else if(vrednost==0) {
+				poteza = poteze.get(i);
+			}
 		}
-		else {
-			ocena = ZMAGA;
-		}
-		
-		List<Koordinati> moznePoteze = igra.moznePoteze();
-		Koordinati kandidat = moznePoteze.get(0);
-				
-		for (Koordinati p: moznePoteze) {
-			
-			Igra kopijaIgre = new Igra(igra);
-			kopijaIgre.odigraj(p);
-			
-			int ocenap;
-			
-			//napisala brez switch da vidim kaj se dogaja
-			
-			/**
-			if (kopijaIgre.stanje() == Stanje.zmaga_rdeci) {
-				if(jaz == Igralec.rdeci) {
-					ocenap = ZMAGA;
-				}
-				else {
-					ocenap = ZGUBA;
-				}
-				
-			}
-			
-			else if (kopijaIgre.stanje() == Stanje.zmaga_modri) {
-				
-				if(jaz == Igralec.modri) {
-					ocenap = ZMAGA;
-				}
-				else {
-					ocenap = ZGUBA;
-				}
-			}
-			else {
-				if (globina == 1) {
-					 ocenap = OceniPozicijo.oceniPozicijo(kopijaIgre, jaz);
-				}
-				else {
-					ocenap = alphabetaPoteze (kopijaIgre, globina-1, alpha, beta, jaz).ocena;
-				}
-			}
-			**/
-			
-			
-			switch (kopijaIgre.stanje()) {
-			case zmaga_rdeci: ocenap = (jaz == Igralec.rdeci ? ZMAGA : ZGUBA); break;
-			case zmaga_modri: ocenap = (jaz == Igralec.modri ? ZMAGA : ZGUBA); break;
-			default:
-				// Nekdo je na potezi
-				if (globina == 1) ocenap = OceniPozicijo.oceniPozicijo(kopijaIgre, jaz);
-				else ocenap = alphabetaPoteze (kopijaIgre, globina-1, alpha, beta, jaz).ocena;
-			}
-		
-			
-			if (igra.naPotezi() == jaz) { // Maksimiramo oceno
-				if (ocenap > ocena) { // mora biti > namesto >=
-					ocena = ocenap;
-					kandidat = p;
-					alpha = Math.max(alpha, ocena);
-				}
-			}
-			else { // igra.naPotezi() != jaz, torej minimiziramo oceno
-				if (ocenap < ocena) { // mora biti < namesto <=
-					ocena = ocenap;
-					kandidat = p;
-					beta = Math.min(beta, ocena);					
-				}	
-			}
-			if (alpha >= beta) // Izstopimo iz "for loop", saj ostale poteze ne pomagajo
-				return new OcenjenaPoteza (kandidat, ocena);
-		//	System.out.println("ocena " + ocena);
-		//	System.out.println("ocenap " + ocenap);
-		}
-		return new OcenjenaPoteza (kandidat, ocena);
-		
+		return poteza;
 	}
 	
 }
--- Hex/src/inteligenca/OceniPozicijo.java
+++ Hex/src/inteligenca/OceniPozicijo.java
@@ -1,5 +1,6 @@
 package inteligenca;
 
+import java.util.List;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -13,177 +14,29 @@ import splosno.Koordinati;
 
 public class OceniPozicijo {
 	
-	// Metoda oceniPozicijo za igro TicTacToe
-	public static int oceniPozicijo(Igra igra, Igralec igralec) {
+	public static int oceniPozicijo(Igra igra, Igralec igralec){
 
-		
-		int ocenap = 0;
-		/**
-		if (igra.stanje() == Stanje.zmaga_rdeci) {
-			if(igralec == Igralec.rdeci) {
-				ocenap = Integer.MAX_VALUE;
-			}
-			else {
-				ocenap = Integer.MIN_VALUE;
-			}
-		}
-		
-		else if (igra.stanje() == Stanje.zmaga_modri) {
-			if(igralec == Igralec.modri) {
-				ocenap = Integer.MAX_VALUE;
-			}
-			else {
-				ocenap = Integer.MIN_VALUE;
-			}
-		}
-		else {
-			for (int i = 0; i < Plosca.N; i++) {
-				
-				
-				int razdalja_rdeci = bfs(igra, Igralec.rdeci);
-				int razdalja_modri = bfs(igra, Igralec.modri);		
-				
-			//	System.out.println("razdalja rdec " + razdalja_rdeci);
-			//	System.out.println("razdalja moder " + razdalja_modri);
+		Igra kopija = new Igra(igra);
+		Polje polje;
+		kopija.naPotezi = igralec.nasprotnik();
+		List<Koordinati> poteze = kopija.moznePoteze();
+		if(poteze.size()==0) {return 0;}
+		for(int i=0; i<poteze.size();i++) {
+			int x = poteze.get(i).getX();
+			int y = poteze.get(i).getX();
+			Polje[][] plosca = kopija.getPlosca();
+			if(kopija.naPotezi == Igralec.rdeci) {polje = Polje.rdece;}
+			else {polje = Polje.modro;}
 			
-				ocenap = (razdalja_rdeci - razdalja_modri);
-				
-		//		System.out.println("rdec-moder " + ocenap);
+			plosca[x][y]=polje;
+			if(kopija.zmagovalnaVrsta != null) {
+				if(igralec == igralec.rdeci) {return 1;}
+				else {return -1;}
 			}
-			
-			if (igralec == Igralec.rdeci) {
-				return ocenap;
-			}
-			else if (igralec == Igralec.modri) {
-				return -ocenap;
-			}			
+			else {oceniPozicijo(kopija, igralec);}
 		}
-		**/
-		
-		switch (igra.stanje()) {
-			case zmaga_rdeci: ocenap += (igralec == Igralec.rdeci ? Integer.MAX_VALUE : Integer.MIN_VALUE); break;
-			case zmaga_modri: ocenap += (igralec == Igralec.modri ? Integer.MAX_VALUE :Integer.MIN_VALUE); break;
-			
-			default:
-
-				int razdalja_rdeci = bfs(igra, Igralec.rdeci);
-				int razdalja_modri = bfs(igra, Igralec.modri);			
-			
-				ocenap = razdalja_rdeci - razdalja_modri;
-				return ocenap;
-				/**
-				if (igralec == Igralec.rdeci) {
-					return ocenap;
-				}
-				else {
-					return -ocenap;
-				}	
-				**/		
-		}
-		return ocenap;
-		
-		//return Integer.MAX_VALUE;
-
-		
+		return 0;
 	}
-	
-	public static int bfs(Igra igra, Igralec igralec) {
-		int[][] smeri = { {0,1}, {0,-1}, {-1,0}, {-1,1}, {1,-1}, {1,0} };
-		boolean[][] visited = new boolean[Plosca.N][Plosca.N];
-		int [][] dolzina = new int[Plosca.N][Plosca.N];
-		
-		for (int i=0; i<Plosca.N; i++) {
-			for (int j=0; j<Plosca.N; j++) {
-				visited[i][j] = false;
-				dolzina[i][j] = Integer.MAX_VALUE;
-			}
-		}
-		
-		if (igralec == Igralec.rdeci) {
-			Queue<Koordinati> queue = new LinkedList<>();
-			for (int i=0; i<Plosca.N; i++) {
-				dolzina[i][0] = 0;
-				visited[i][0] = true;
-				queue.add(new Koordinati(i, 0));
-			}
-			
-			 
-			while (queue.size() > 0) {
-				Koordinati vozlisce = queue.remove();
-				int vozliscex = vozlisce.getX();
-				int vozliscey = vozlisce.getY();
-				
-				for (int j=0; j<smeri.length; j++) {
-					int sosedx = vozliscex + smeri[j][0];
-					int sosedy = vozliscey+ smeri[j][1];
-					Koordinati sosed = new Koordinati(sosedx, sosedy);
-					
-					if (sosedx>=0 && sosedy >= 0 && sosedx<Plosca.N && sosedy<Plosca.N && !visited[sosedx][sosedy]) {
-						visited[sosedx][sosedy] = true;
-						
-						if (igra.plosca.plosca[sosedx][sosedy] == Polje.rdece) {
-							dolzina[sosedx][sosedy] = Math.min(dolzina[sosedx][sosedy], dolzina[vozliscex][vozliscey]);
-	                        queue.add(sosed);
-						}
-						
-						else if (igra.plosca.plosca[sosedx][sosedy] == Polje.prazno) {
-							dolzina[sosedx][sosedy] = Math.min(dolzina[sosedx][sosedy], dolzina[vozliscex][vozliscey] + 1);
-	                        queue.add(sosed);
-						}
-						
-						else {
-							dolzina[sosedx][sosedy] = Integer.MAX_VALUE;
-						}
-						
-						if (sosedy == Plosca.N - 1) {
-							return dolzina[sosedx][sosedy];
-						}
-					}
-				}	 
-			}
-			return Integer.MAX_VALUE;
-		}
-		
-		else {
-			Queue<Koordinati> queue = new LinkedList<>();
-			for (int i=0; i<Plosca.N; i++) {
-				dolzina[0][i] = 0;
-				visited[0][i] = true;
-				queue.add(new Koordinati(0, i));
-			}
-			 
-			while (queue.size() > 0) {
-				Koordinati vozlisce = queue.remove();
-				int vozliscex = vozlisce.getX();
-				int vozliscey = vozlisce.getY();
-				for (int j=0; j<smeri.length; j++) {
-					int sosedx = vozliscex + smeri[j][0];
-					int sosedy = vozliscey+ smeri[j][1];
-					Koordinati sosed = new Koordinati(sosedx, sosedy);
-					if (sosedx>=0 && sosedy >= 0 && sosedx<Plosca.N && sosedy<Plosca.N && !visited[sosedx][sosedy]) {
-						visited[sosedx][sosedy] = true;
-						if (igra.plosca.plosca[sosedx][sosedy] == Polje.modro) {
-							dolzina[sosedx][sosedy] = Math.min(dolzina[sosedx][sosedy], dolzina[vozliscex][vozliscey]);
-	                        queue.add(sosed);
-						}
-						else if (igra.plosca.plosca[sosedx][sosedy] == Polje.prazno) {
-							dolzina[sosedx][sosedy] = Math.min(dolzina[sosedx][sosedy], dolzina[vozliscex][vozliscey] + 1);
-	                        queue.add(sosed);
-						}
-						else {
-							dolzina[sosedx][sosedy] = Integer.MAX_VALUE;
-						}
-						
-						if (sosedx == Plosca.N - 1) {
-							return dolzina[sosedx][sosedy];
-						}
-					}
-				}	 
-			}
-		}
-		return Integer.MAX_VALUE;
-	}
-	
 }
 
 
